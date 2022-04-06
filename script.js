285f98ccf6a5240cf9bfc61b75ebb90ad736cf6e
GophTheGreat <13459026+GophTheGreat@users.noreply.github.com>
2022-04-05 15:49:28 -1000

    redid entire thing works now

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let num2 = "";
 let operator = "";
 let working = "";
 let answer = "";
-
+let active = "num1";
 let inEqualsLoop = false;
 
 function add(a, b){
@@ -53,147 +53,137 @@ const clearButton = document.querySelector(".clearButton");
 const sqrtButton = document.querySelector(".sqrtButton");
 const decimalButton = document.querySelector("#decimalButton");
 const workZone = document.querySelector("#workZone");
-
-workZone.textContent = working;
-
+workZone.textContent = "";
 
 
-//Numerical buttons
-for(let i = 0; i < numButtons.length; i++){
-  numButtons[i].addEventListener("click", (e) => {
-    if(answer){
-      answer = "";
-      workZone.textContent += e.target.textContent;
-      working = workZone.textContent;
-      num1 = "";
-      num2 = "";
-      operator = "";
-      console.log(`Clicked numeral "${e.target.textContent}" with prev answer. State is <${num1} ${operator} ${num2}> working = ${working}`);
-      console.log("Exiting equals");
-      inEqualsLoop = false;
-      return;
-    }
-    if(num2 != ""){
-      working = workZone.textContent;
-      num2 = "";
-      num1 = "";
-    }
-    if(working == "0" || working == ""){
-      workZone.textContent = e.target.textContent;
-      working = e.target.textContent;
-    }
-    else{
-      workZone.textContent += e.target.textContent;
-      working += e.target.textContent;
-    }
-    console.log(`Clicked numeral "${e.target.textContent}"  <${num1} ${operator} ${num2}> working = ${working}`);
-    });
-}
+for(let i = 0; i < numButtons.length; i++){numButtons[i].addEventListener("click", appendNumber)};
+for(let i = 0; i < opButtons.length; i++){opButtons[i].addEventListener("click", appendOperator)};
+eqButton.addEventListener("click", equals);
+clearButton.addEventListener("click", clear);
+decimalButton.addEventListener("click", appendDecimal);
 
-//Operator buttons
-for(let i = 0; i < opButtons.length; i++){
-  opButtons[i].addEventListener("click", (e) => {
-    if(operator == ""){
-      operator = e.target.textContent;
-      if(num1 == "" || num1 == "0"){
-        if(inEqualsLoop == true){
-          inEqualsLoop = false;
-          console.log("Exiting equals");
-          updateDecimalInactive(decimalButton);
-        };
-        num1 = workZone.textContent;
-        console.log(`Operator set as ${e.target.textContent}`);
-        working = "";
-      }
-      working = "";
-    } 
-    else {
-      if(inEqualsLoop == true)
-      {
-        num2 = working;
-        operator = e.target.textContent;
-        working = "";
-        inEqualsLoop = false;
-        console.log("Exiting equals");
-        updateDecimalInactive(decimalButton);
-      };
-      solve();
-      operator = e.target.textContent;
-    };
-    updateDecimalInactive(decimalButton);
-    console.log("Clearing answer")
-    answer = "";
-    console.log(`Clicked operator <${num1} ${operator} ${num2}> working = ${working}`);
-  });
-}
-
-eqButton.addEventListener("click", () => {
-      console.log(`Clicked "equals" <${num1} ${operator} ${num2}> working = ${working}`);
-  if(num1 && operator){
+function equals(){
+  console.log(`Clicked "equals" <${num1} ${operator} ${num2}> working = ${working}`);
+  if(inEqualsLoop == true){
+    num1 = workZone.textContent;
     answer = solve();
     workZone.textContent = answer;
-    working = num2;
-    inEqualsLoop = true;
-    updateDecimalInactive(decimalButton);
+    working = answer;
+    return;
+  }
+  if(active == "num1")
+    num1 = working;
+  if(active == "num2")
+    num2 = working;
+  answer = solve();
+  workZone.textContent = answer;
+  working = answer;
+  inEqualsLoop = true;
+}
+
+function appendNumber(){
+  if(working == '0' || working == "")
+    working = this.textContent;
+  else
+    working += this.textContent;
+  workZone.textContent = working;
+  console.log(`Pressed button "${this.textContent}". Current status: <${num1} ${operator} ${num2}> working = ${working}`); 
+}
+
+function appendDecimal(){
+  if(working.includes(".")){
+    if(!this.classList.contains("inactive"))
+      this.classList.add("inactive");
     return;
   }
   else{
+    if(this.classList.contains("inactive"))
+      this.classList.remove("inactive");
+    workZone.textContent += this.textContent;
+    working += this.textContent;
+    this.classList.add("inactive");
     return;
+  } 
+}
+
+function appendOperator(){
+  console.log(`Active is ${active}`);
+  if(this.textContent != operator && operator != ""){
+    if(!inEqualsLoop)
+    {
+      num2 = workZone.textContent;
+      answer = solve();
+      num1 = answer;
+      workZone.textContent = answer;
+      active = "num2";
+      working = "";
+      operator = this.textContent;
+      inEqualsLoop = "false";
+      return;
+    }
+    else{
+      num1 = workZone.textContent;
+      working = "";
+      active = "num2";
+      operator = this.textContent;
+      inEqualsLoop = "false";
+      return;
+    }
   }
-});
+  operator = this.textContent;
+  if(active == "num1"){
+    num1 = workZone.textContent;
+    active = "num2";
+    working = "";
+  }
+  else if(active == "num2"){
+    num2 = workZone.textContent;
+    answer = solve();
+    num1 = answer;
+    workZone.textContent = answer;
+    active = "num2";
+  }
+  working = "";
+  inEqualsLoop = "false";
+  console.log(`Appending operator ${this.textContent}\nActive = ${active}`)
+  console.log(`Current status: <${num1} ${operator} ${num2}> working = ${working}`); 
+}
+
 
 function solve(){
-  num2 = working;
   console.log(`Solve evaluating ${num1} ${operator} ${num2}`);
   if(num1 && num2){
     console.log(`Solving...`);
     answer = operate(operator, num1, num2);
-    num1 = answer;
-    workZone.textContent = answer;
-    working = "";
     updateDecimalInactive(decimalButton);
-    console.log(`Ending with ${num1} ${operator} ${num2} \n`);
+    console.log(`Ending with ${num1} ${operator} ${num2} = ${answer} \n\n\n`);
     return answer;
   }
   console.log(`Skipping solve`);
   return;
 }
 
-clearButton.addEventListener("click", () => {
-  num1 = "0";
+function clear(){
+  num1 = "";
   num2 = "";
   operator = "";
   answer = "";
   working = "0";
   inEqualsLoop = false;
-  workZone.textContent = working;
-});
+  workZone.textContent = "0";
+  active = "num1";
+}
 
-decimalButton.addEventListener("click", (e) => {
-  if(working.includes(".")){
-    if(!e.target.classList.contains("inactive"))
-      e.target.classList.add("inactive");
+function updateDecimalInactive(decimalButton){
+  console.log(`Updating decimal. Working is "${working}" and type of ${typeof working}`);
+  if(toString(working).includes(".")){
+    if(!decimalButton.classList.contains("inactive"))
+      decimalButton.classList.add("inactive");
     return;
   }
   else{
-    if(e.target.classList.contains("inactive"))
-      e.target.classList.remove("inactive");
-    workZone.textContent += e.target.textContent;
-    working += e.target.textContent;
-    e.target.classList.add("inactive");
-    return;
-  } 
-});
-
-function updateDecimalInactive(button){
-  if(working.includes(".")){
-    if(!button.classList.contains("inactive"))
-      button.classList.add("inactive");
-    return;
-  }
-  else{
-    if(button.classList.contains("inactive"))
-      button.classList.remove("inactive");
+    if(decimalButton.classList.contains("inactive"))
+      decimalButton.classList.remove("inactive");
     return;
   }
 }
